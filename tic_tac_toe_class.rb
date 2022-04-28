6816fede509cd783c4cdffe73bfdc30bf9c1c409
brianhasal <104147509+brianhasal@users.noreply.github.com>
2022-04-28 06:01:21 -0700

    Create tic_tac_toe_class.rb
    It still haunts me...

---
+++ tic_tac_toe_class.rb
@@ -0,0 +1,648 @@
+block1x = [
+  "                   #",
+  "    XXX     XXX    #",
+  "     XXX   XXX     #",
+  "      XXX XXX      #",
+  "       XXXXX       #",
+  "      XXX XXX      #",
+  "     XXX   XXX     #",
+  "    XXX     XXX    #",
+  "                   #",
+  "####################"
+]
+
+
+block1o = [
+  "                   #",
+  "       OOOOO       #",
+  "     OOO   OOO     #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "     OOO   OOO     #",
+  "       OOOOO       #",
+  "                   #",
+  "####################"
+]
+
+block1n = [
+  "                   #",
+  "        111        #",
+  "     111111        #",
+  "        111        #",
+  "        111        #",
+  "        111        #",
+  "        111        #",
+  "     111111111     #",
+  "                   #",
+  "####################"
+]
+
+
+block2x = [
+  "#                   #",
+  "#    XXX     XXX    #",
+  "#     XXX   XXX     #",
+  "#      XXX XXX      #",
+  "#       XXXXX       #",
+  "#      XXX XXX      #",
+  "#     XXX   XXX     #",
+  "#    XXX     XXX    #",
+  "#                   #",
+  "#####################"
+]
+
+block2o = [
+  "#                   #",
+  "#       OOOOO       #",
+  "#     OOO   OOO     #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#     OOO   OOO     #",
+  "#       OOOOO       #",
+  "#                   #",
+  "#####################"
+]
+
+block2n = [
+  "#                   #",
+  "#        2222       #",
+  "#      222  222     #",
+  "#           222     #",
+  "#          222      #",
+  "#         222       #",
+  "#       222         #",
+  "#      22222222     #",
+  "#                   #",
+  "#####################"
+]
+
+block3x = [
+  "#                   ",
+  "#    XXX     XXX    ",
+  "#     XXX   XXX     ",
+  "#      XXX XXX      ",
+  "#       XXXXX       ",
+  "#      XXX XXX      ",
+  "#     XXX   XXX     ",
+  "#    XXX     XXX    ",
+  "#                   ",
+  "####################"
+]
+
+block3o = [
+  "#                   ",
+  "#       OOOOO       ",
+  "#     OOO   OOO     ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#     OOO   OOO     ",
+  "#       OOOOO       ",
+  "#                   ",
+  "####################"
+]
+
+block3n = [
+  "#                   ",
+  "#        3333       ",
+  "#      333  333     ",
+  "#           333     ",
+  "#         333       ",
+  "#           333     ",
+  "#      333  333     ",
+  "#        3333       ",
+  "#                   ",
+  "####################"
+]
+
+block4x = [
+  "                   #",
+  "    XXX     XXX    #",
+  "     XXX   XXX     #",
+  "      XXX XXX      #",
+  "       XXXXX       #",
+  "      XXX XXX      #",
+  "     XXX   XXX     #",
+  "    XXX     XXX    #",
+  "                   #",
+  "####################"
+]
+
+
+block4o = [
+  "                   #",
+  "       OOOOO       #",
+  "     OOO   OOO     #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "     OOO   OOO     #",
+  "       OOOOO       #",
+  "                   #",
+  "####################"
+]
+
+block4n = [
+  "                   #",
+  "         444       #",
+  "        4444       #",
+  "       44 44       #",
+  "      44  44       #",
+  "     44444444      #",
+  "          44       #",
+  "          44       #",
+  "                   #",
+  "####################"
+]
+
+block5x = [
+  "#                   #",
+  "#    XXX     XXX    #",
+  "#     XXX   XXX     #",
+  "#      XXX XXX      #",
+  "#       XXXXX       #",
+  "#      XXX XXX      #",
+  "#     XXX   XXX     #",
+  "#    XXX     XXX    #",
+  "#                   #",
+  "#####################"
+]
+
+block5o = [
+  "#                   #",
+  "#       OOOOO       #",
+  "#     OOO   OOO     #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#     OOO   OOO     #",
+  "#       OOOOO       #",
+  "#                   #",
+  "#####################"
+]
+
+block5n = [
+  "#                   #",
+  "#      55555555     #",
+  "#      55           #",
+  "#      55           #",
+  "#      5555555      #",
+  "#            55     #",
+  "#            55     #",
+  "#      5555555      #",
+  "#                   #",
+  "#####################"
+]
+
+block6x = [
+  "#                   ",
+  "#    XXX     XXX    ",
+  "#     XXX   XXX     ",
+  "#      XXX XXX      ",
+  "#       XXXXX       ",
+  "#      XXX XXX      ",
+  "#     XXX   XXX     ",
+  "#    XXX     XXX    ",
+  "#                   ",
+  "####################"
+]
+
+block6o = [
+  "#                   ",
+  "#       OOOOO       ",
+  "#     OOO   OOO     ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#     OOO   OOO     ",
+  "#       OOOOO       ",
+  "#                   ",
+  "####################"
+]
+
+block6n = [
+  "#                   ",
+  "#         666       ",
+  "#        666        ",
+  "#       666         ",
+  "#      6666666      ",
+  "#     666   666     ",
+  "#     666   666     ",
+  "#      6666666      ",
+  "#                   ",
+  "####################"
+]
+
+
+block7x = [
+  "                   #",
+  "    XXX     XXX    #",
+  "     XXX   XXX     #",
+  "      XXX XXX      #",
+  "       XXXXX       #",
+  "      XXX XXX      #",
+  "     XXX   XXX     #",
+  "    XXX     XXX    #",
+  "                   #"
+  
+]
+
+
+block7o = [
+  "                   #",
+  "       OOOOO       #",
+  "     OOO   OOO     #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "    OOO     OOO    #",
+  "     OOO   OOO     #",
+  "       OOOOO       #",
+  "                   #"
+  
+]
+
+block7n = [
+  "                   #",
+  "    777777777777   #",
+  "    777     777    #",
+  "           777     #",
+  "        7777777    #",
+  "         777       #",
+  "        777        #",
+  "       777         #",
+  "                   #"
+  
+]
+
+block8x = [
+  "#                   #",
+  "#    XXX     XXX    #",
+  "#     XXX   XXX     #",
+  "#      XXX XXX      #",
+  "#       XXXXX       #",
+  "#      XXX XXX      #",
+  "#     XXX   XXX     #",
+  "#    XXX     XXX    #",
+  "#                   #"
+  
+]
+
+block8o = [
+  "#                   #",
+  "#       OOOOO       #",
+  "#     OOO   OOO     #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#    OOO     OOO    #",
+  "#     OOO   OOO     #",
+  "#       OOOOO       #",
+  "#                   #"
+]
+
+block8n = [
+  "#                   #",
+  "#       88888       #",
+  "#      88   88      #",
+  "#       88 88       #",
+  "#      8888888      #",
+  "#     888   888     #",
+  "#     888   888     #",
+  "#      8888888      #",
+  "#                   #"
+]
+
+block9x = [
+  "#                   ",
+  "#    XXX     XXX    ",
+  "#     XXX   XXX     ",
+  "#      XXX XXX      ",
+  "#       XXXXX       ",
+  "#      XXX XXX      ",
+  "#     XXX   XXX     ",
+  "#    XXX     XXX    ",
+  "#                   "
+]
+
+block9o = [
+  "#                   ",
+  "#       OOOOO       ",
+  "#     OOO   OOO     ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#    OOO     OOO    ",
+  "#     OOO   OOO     ",
+  "#       OOOOO       ",
+  "#                   "
+]
+
+block9n = [
+  "#                   ",
+  "#      9999999      ",
+  "#     999   999     ",
+  "#     999   999     ",
+  "#      9999999      ",
+  "#         999       ",
+  "#        999        ",
+  "#       999         ",
+  "#                   "
+]
+
+block1_array = [block1n, block1o, block1x]
+block2_array = [block2n, block2o, block2x]
+block3_array = [block3n, block3o, block3x]
+
+block4_array = [block4n, block4o, block4x]
+block5_array = [block5n, block5o, block5x]
+block6_array = [block6n, block6o, block6x]
+
+block7_array = [block7n, block7o, block7x]
+block8_array = [block8n, block8o, block8x]
+block9_array = [block9n, block9o, block9x]
+
+
+
+
+# line_counter = 0
+# print block2xl[line_counter]
+
+# x_a = [block1x, block2x]
+
+# block1xl.each do |line|
+
+# puts "#{block1xl}#{block2x}"
+
+# print block1o
+# print block1n
+
+def row_printer(array0, array1, array2)
+  line_counter = 0
+  array0.each do |line| 
+    puts "#{line}#{array1[line_counter]}#{array2[line_counter]}"
+    line_counter += 1
+  end
+end
+
+# row_printer(block1x, block2n, block3o)
+# row_printer(block4n, block5x, block6n)
+# row_printer(block7o, block8n, block9x)
+
+
+
+
+class BoardPiece
+  def initialize(box_number, states_array)
+    @box_number = box_number
+    @states_array = states_array
+    @current_state = "neutral"
+  end
+
+  def box_number
+    @box_number
+  end
+
+  def states_array
+    @states_array
+  end
+
+
+  def current_state
+    @current_state
+  end
+
+
+  def current_state=(input)
+    @current_state = input
+  end
+
+  def display
+    if @current_state == "oplayer"
+      return states_array[1]
+    elsif @current_state == "xplayer"
+      return states_array[2]
+    else 
+      return states_array[0]
+    end 
+  end
+
+end
+
+
+
+bp1 = BoardPiece.new(1, block1_array)
+bp2 = BoardPiece.new(2, block2_array)
+bp3 = BoardPiece.new(3, block3_array)
+bp4 = BoardPiece.new(4, block4_array)
+bp5 = BoardPiece.new(5, block5_array)
+bp6 = BoardPiece.new(6, block6_array)
+bp7 = BoardPiece.new(7, block7_array)
+bp8 = BoardPiece.new(8, block8_array)
+bp9 = BoardPiece.new(9, block9_array)
+
+# p bp1.class
+
+b1d = bp1.display
+b2d = bp2.display
+b3d = bp3.display
+b4d = bp4.display
+b5d = bp5.display
+b6d = bp6.display
+b7d = bp7.display
+b8d = bp8.display
+b9d = bp9.display
+
+# bp_display_array = [bp1.display, bp2.display, bp3.display, bp4.display, bp5.display, bp6.display, bp7.display, bp8.display, bp9.display]
+
+
+
+def display_arrays()
+  b1d = bp1.display
+  b2d = bp2.display
+  b3d = bp3.display
+  b4d = bp4.display
+  b5d = bp5.display
+  b6d = bp6.display
+  b7d = bp7.display
+  b8d = bp8.display
+  b9d = bp9.display
+end
+
+def dh_refresh(display_hashes)
+  display_hashes[b1d] = bp1.display
+  display_hashes[b2d] = bp2.display
+  display_hashes[b3d] = bp3.display
+  display_hashes[b4d] = bp4.display
+  display_hashes[b5d] = bp5.display
+  display_hashes[b6d] = bp6.display
+  display_hashes[b7d] = bp7.display
+  display_hashes[b8d] = bp8.display
+  display_hashes[b9d] = bp9.display
+end
+
+
+# display_hashes = [
+#   b1d = bp1.display,
+#   b2d = bp2.display,
+#   b3d = bp3.display,
+#   b4d = bp4.display,
+#   b5d = bp5.display,
+#   b6d = bp6.display,
+#   b7d = bp7.display,
+#   b8d = bp8.display,
+#   b9d = bp9.display
+# ]
+
+def condense(*args)
+  array = []
+  args.each do |arg|
+    array << arg
+  end
+  array
+end
+
+board_array = condense(b1d, b2d, b3d, b4d, b5d, b6d, b7d, b8d, b9d)
+
+puts board_array
+
+# def display_ba(array)
+#   row_printer(array[0], array[1], array[2])
+#   row_printer(array[3], array[4], array[5])
+#   row_printer(array[6], array[7], array[8])
+# end
+
+
+def display (d1, d2, d3, d4, d5, d6, d7, d8, d9)
+  row_printer(d1, d2, d3)
+  row_printer(d4, d5, d6)
+  row_printer(d7, d8, d9)
+end
+
+display(bp1.display, bp2.display, bp3.display, bp4.display, bp5.display, bp6.display, bp7.display, bp8.display, bp9.display)
+
+# display_ba(board_array)
+
+
+########### BIG TAKEAWAY #############
+
+
+
+#####  A KEY/VALUE STATEMENT, WHERE THE KEY IS A VARIABLE, AND THE VALUE CALLS A METHOD ON A CLASS OBJECT, BOTH OF WHICH ARE CONTAINED IN AN ARRAY, THE VALUE IS NOT UPDATED TO ITS CURRENT VALUE, RATHER THE VALUE IS ITS INITIAL VALUE. CALLING THE KEY WILL ONLY RETURN THE INITIAL VALUE ASSIGNED TO THE VALUE. THE ELEMENT DOES NOT UPDATE INSIDE AN ARRAY, NOR DOES A VALUE UPDATE INSIDE A HASH'S KEY.
+
+
+bp7.current_state = "oplayer"
+
+p bp7.current_state
+
+# b7d = bp7.display
+
+# display_ba(board_array)
+
+display(bp1.display, bp2.display, bp3.display, bp4.display, bp5.display, bp6.display, bp7.display, bp8.display, bp9.display)
+
+
+board_array = condense(b1d, b2d, b3d, b4d, b5d, b6d, b7d, b8d, b9d)
+
+puts board_array
+
+
+
+
+
+# dh_refresh(display_hashes)
+
+
+
+# display(display_hashes.keys)
+
+
+
+# puts display_hashes.class
+# puts display_hashes[b7d].class
+# puts display_hashes.keys
+
+# display(display_hashes.keys)
+
+
+
+
+
+
+
+array = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]]
+
+
+
+
+def board_maker(array)
+  print array[0][0]
+  print array[0][1]
+  puts array[0][2]
+  print array[1][0]
+  print array[1][1]
+  puts array[1][2]
+  print array[2][0]
+  print array[2][1]
+  puts array[2][2]
+end
+
+# board_maker(array)
+
+
+
+
+def tic_tac_toe()
+
+
+
+
+
+
+
+
+
+
+
+  def each_round(array, turn_bool)
+    x = "X"
+    o = "O"
+    piece = ""
+    input_options = [1...9]
+    did_drop = false
+    updated_array = array
+  
+    if turn_bool == false
+      piece = x
+      puts "Player One, it's your turn."
+    else
+      piece = o
+      puts "Player Two, it's your turn."
+    end
+  
+  
+    while did_drop == false
+      did_drop = true
+      input_test = false
+      while input_test == false
+        print "Please enter a column to drop your piece (1 - #{last_no.to_s}): "
+        user_input = gets.chomp
+  
+        if input_options.include?(user_input.to_i) == true
+          input_test = true
+        end
+      end
+      column = user_input.to_i
+      results = piece_dropper(updated_array, column, piece)
+      did_drop = results[1]
+      updated_array = results[0]
+    end
+  
+    make_board(updated_array)
+    return updated_array
+  
+  end
+
+
+
+
+end
